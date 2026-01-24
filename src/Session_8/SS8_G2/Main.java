9cd21d5e23182e3b96a1b0192001c10b5ea38284
NMC2007 <nmc281207@gmail.com>
2026-01-24 19:58:14 +0700

    Update SS8 G2

---
+++ src/Session_8/SS8_G2/Main.java
@@ -0,0 +1,17 @@
+package Session_8.SS8_G2;
+
+public class Main {
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        Rectangle r1 = new Rectangle(3, 4);
+        Rectangle r2 = new Rectangle(5, 2);
+        Rectangle r3 = new Rectangle(4.5, 3.5);
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+    }
+}
